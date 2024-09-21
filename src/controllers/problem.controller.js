a63fd4956c3a498b1587fc307822640e6ec76218
Aviraj Kumar <aviraj4777@gmail.com>
2024-09-21 12:13:08 +0530

    Basic Error Handling Classes Added

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,27 +1,48 @@
 const { statusCodes } = require('http-status-code');
+const NotImplemented = require('../errors/notImplemented.error');
 
 function pingProblemController(req, res) {
     return res.json({message: 'Problem Controller is up'});
 }
 
-function addProblem(req, res) {
-    return res.status(statusCodes.NOT_IMPLEMENTED).json({message : 'Not Implemented'});
+function addProblem(req, res, next) {
+    try {
+        throw new NotImplemented('Add Problem');
+    } catch (error) {
+        next(error);
+    }
 }
 
-function getProblem(req, res) {
-    return res.status(statusCodes.NOT_IMPLEMENTED).json({message : 'Not Implemented'});
+function getProblem(req, res, next) {
+    try {
+        throw new NotImplemented('Get Problem');
+    } catch (error) {
+        next(error);
+    }
 }
 
-function getProblems(req, res) {
-    return res.status(statusCodes.NOT_IMPLEMENTED).json({message : 'Not Implemented'});
+function getProblems(req, res, next) {
+    try {
+        throw new NotImplemented('Get Problems');
+    } catch (error) {
+        next(error);
+    }
 }
 
-function deleteProblem(req, res) {
-    return res.status(statusCodes.NOT_IMPLEMENTED).json({message : 'Not Implemented'});
+function deleteProblem(req, res, next) {
+    try {
+        throw new NotImplemented('Delete Problem');
+    } catch (error) {
+        next(error);
+    }
 }
 
-function updateProblem(req, res) {
-    return res.status(statusCodes.NOT_IMPLEMENTED).json({message : 'Not Implemented'});
+function updateProblem(req, res, next) {
+    try {
+        throw new NotImplemented('Update Problem');
+    } catch (error) {
+        next(error);
+    }
 }
 
 module.exports = {
