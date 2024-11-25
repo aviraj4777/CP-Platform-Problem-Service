2532405f1b16316804d4ed90bcb93a1be003bdc1
Aviraj Kumar <aviraj4777@gmail.com>
2024-11-25 09:44:18 +0530

    Feat : Added deleteProblem and updateProblem API

--- src/repositories/problem.repository.js
+++ src/repositories/problem.repository.js
@@ -1,32 +1,63 @@
-const { Problem } = require('../models');
+const NotFound = require("../errors/notFound.error");
+const { Problem } = require("../models");
 
 class ProblemRepository {
-    async createProblem(problemData) {
-        try {
-            // create method of mongoose creates model and save it to the document;
-            const problem = await Problem.create({
-                title: problemData.title,
-                description: problemData.description,
-                testCases: (problemData.testCases) ? problemData.testCases : []
-            });
+  async createProblem(problemData) {
+    try {
+      // create method of mongoose creates model and save it to the document (database);
+      const problem = await Problem.create({
+        title: problemData.title,
+        description: problemData.description,
+        testCases: problemData.testCases ? problemData.testCases : [],
+      });
 
-            return problem;
-        }
-        catch(error) {
-            console.log(error);
-            throw error;
-        }
+      return problem;
+    } catch (error) {
+      console.log(error);
+      throw error;
     }
-    async getAllProblems() {
-        try {
-            const problems = await Problem.find({});
-            return problems;
-        }
-        catch(error) {
-            console.log(error);
-            throw error;
-        }
+  }
+  async getAllProblems() {
+    try {
+      const problems = await Problem.find({});
+      return problems;
+    } catch (error) {
+      console.log(error);
+      throw error;
     }
+  }
+  async getProblem(id) {
+    try {
+      const problem = await Problem.findById(id);
+      if (!problem) throw new NotFound("Problem", id);
+      return problem;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+  async deleteProblem(id) {
+    try {
+      const res = await Problem.deleteOne({ _id: id });
+      if (!res.deletedCount) throw new NotFound("Problem", id);
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+  async updateProblem(id, update) {
+    try {
+      const oldDoc = await Problem.findById(id);
+      if (!oldDoc) throw new NotFound("Problem", id);
+      for (const key in update) {
+        oldDoc[key] = update[key];
+      }
+      await oldDoc.save();
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
 }
 
 module.exports = ProblemRepository;
